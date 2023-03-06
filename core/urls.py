519dd2a9dec49cb740609eb5bcacd226a53044c5
Usver <kot613@gmail.com>
2023-03-06 10:46:53 +0200

    add drf-yasg

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as pattern
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('api.urls')),
 ]
+
+urlpatterns += pattern
